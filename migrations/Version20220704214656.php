5af9dde0e277522e4bd2f8571796813eecb32a97
amare16 <amarenighatu16@gmail.com>
2022-07-04 23:48:22 +0200

    add migrations

--- migrations/Version20220704214656.php
+++ migrations/Version20220704214656.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220704210648 extends AbstractMigration
+final class Version20220704214656 extends AbstractMigration
 {
     public function getDescription(): string
     {
